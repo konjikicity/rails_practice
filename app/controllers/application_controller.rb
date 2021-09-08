9c6acf6a1fa602f9724b0ffb578b2a4384b8d7bc
Syu <konjikicity@gmail.com>
2021-09-08 21:30:11 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login #sorceryが作成するメソッド。ログインしてない時not_authenticatedメソッドを発火する
+
+  protected
+  
+  def not_authenticated
+    redirect_to login_url
+  end
 end
