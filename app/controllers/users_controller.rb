51420dcb19d46ea94df15cb681ae80408ecccd15
Syu <konjikicity@gmail.com>
2021-09-09 10:36:52 +0900

    flashメッセージの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, success: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
